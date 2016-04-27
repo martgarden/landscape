2ef2f007de8ac071e091fa89cce1b1b9e3fddcba
Martin Zahradnicek <xzahrad3@nymfe16.fi.muni.cz>
2016-04-27 12:33:15 +0200

    light.hpp mostly done.

--- light.hpp
+++ light.hpp
@@ -4,11 +4,19 @@
 namespace marrow {
     class Light {
         private:
-            struct ubo_data {
-                glm::vec4
+            struct _ubo_structure {
+                float _position[4];
+                float _diffuse[4];
+                float _ambient[4];
+                float _specular[4];
+            } _ubo_data;
+            Gluint _ubo_id;
 
         public:
-            Light();
+            Light(glm::vec4 position, glm::vec3 diffuse, glm::vec3 ambient, glm::vec3 specular);
+
+            getUBO();
+
     };
 }
 
