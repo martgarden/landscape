c45d957a69b7617b4ab64ad94a8669c2e64df958
Martin Zahradníček <sagrada@debiman>
2016-04-27 11:59:18 +0200

    added light base

---
+++ light.hpp
@@ -0,0 +1,15 @@
+#ifndef MARROW_LIGHT_H
+#define MARROW_LIGHT_H
+
+namespace marrow {
+    class Light {
+        private:
+            struct ubo_data {
+                glm::vec4
+
+        public:
+            Light();
+    };
+}
+
+#endif
