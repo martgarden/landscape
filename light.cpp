1ffcbecdeddec6b59f296eb75292e7c8bd95e760
Martin Zahradnicek <xzahrad3@nymfe16.fi.muni.cz>
2016-04-27 13:09:56 +0200

    Lights done.

---
+++ light.cpp
@@ -0,0 +1,20 @@
+#include "light.hpp"
+
+namespace marrow {
+    Light NO_LIGHT(glm::vec4(0.0f), glm::vec3(0.0f), glm::vec3(0.0f), glm::vec3(0.0f));
+
+    Light::Light(glm::vec4 & position, glm::vec3 & diffuse, glm::vec3 & ambient, glm::vec3 & specular) {
+        glGenBuffers(1, &_ubo_id);
+        glBindBuffer(GL_UNIFORM_BUFFER, _ubo_id);
+        memcpy(_ubo_data.position, glm::value_ptr(position), sizeof(GLfloat)*4);
+        memcpy(_ubo_data.diffuse, glm::value_ptr(diffuse), sizeof(GLfloat)*3);
+        memcpy(_ubo_data.amibient, glm::value_ptr(amibient), sizeof(GLfloat)*3);
+        memcpy(_ubo_data.specular, glm::value_ptr(specular), sizeof(GLfloat)*3);
+        glBufferData(GL_UNIFORM_BUFFER, 16*sizeof(GLfloat), &_ubo_data, GL_STATIC_DRAW);
+        glBindBuffer(GL_UNIFORM_BUFFER, 0);
+    }
+
+    Gluint Light::getUBO() {
+        return _ubo_id;
+    }
+}
--- light.hpp
+++ light.hpp
@@ -5,19 +5,21 @@ namespace marrow {
     class Light {
         private:
             struct _ubo_structure {
-                float _position[4];
-                float _diffuse[4];
-                float _ambient[4];
-                float _specular[4];
+                GLfloat _position[4];
+                GLfloat _diffuse[4];
+                GLfloat _ambient[4];
+                GLfloat _specular[4];
             } _ubo_data;
             Gluint _ubo_id;
 
         public:
-            Light(glm::vec4 position, glm::vec3 diffuse, glm::vec3 ambient, glm::vec3 specular);
+            Light(glm::vec4 & position, glm::vec3 & diffuse, glm::vec3 & ambient, glm::vec3 & specular);
 
-            getUBO();
+            Gluint getUBO();
 
     };
+
+    extern Light NO_LIGHT;
 }
 
 #endif
