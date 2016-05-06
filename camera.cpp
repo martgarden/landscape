0b45ad55e8ca4b81aef8b44c76537ca3306a8e35
Martin Zahradníček <martin.zahradniceks@gmail.com>
2016-05-06 16:23:10 +0200

    Basic camera.

--- camera.cpp
+++ camera.cpp
@@ -3,10 +3,24 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 namespace marrow {
+    void Camera::update() {
+        _direction = glm::mat3(glm::rotate(glm::mat4(1.0), _rotation, glm::vec3(0.0f, 1.0f, 0.0f)))*
+                     glm::mat3(glm::rotate(glm::mat4(1.0), _elevation, glm::vec3(1.0f, 0.0f, 0.0f)))*
+                     glm::vec3(0.0f, 0.0f, 1.0f);
+    }
+
     Camera::Camera(glm::vec3 eye_position, glm::vec3 direction, glm::vec3 normal) {
         _eye_position = eye_position;
         _direction = direction;
         _normal = normal;
+    }
+
+    Camera::Camera(glm::vec3 eye_position, float rotation, float elevation) {
+        _eye_position = eye_position;
+        _normal = glm::vec3(0.0, 1.0, 0.0);
+        _rotation = rotation;
+        _elevation = elevation;
+        update();
     }
 
     glm::vec3 Camera::getEyePosition() {
@@ -15,5 +29,24 @@ namespace marrow {
 
     glm::mat4 Camera::getViewMatrix() {
         return glm::lookAt(_eye_position, _eye_position + _direction, _normal);
+    }
+    void Camera::left(float time) {
+        _rotation -= time * _rotation_speed;
+        update();
+    }
+
+    void Camera::right(float time) {
+        _rotation += time * _rotation_speed;
+        update();
+    }
+
+    void Camera::up(float time) {
+        _elevation += time * _rotation_speed;
+        update();
+    }
+
+    void Camera::down(float time) {
+        _elevation -= time * _rotation_speed;
+        update();
     }
 }
--- camera.hpp
+++ camera.hpp
@@ -7,14 +7,24 @@ namespace marrow {
     class Camera {
         protected:
             glm::vec3 _eye_position;
+            float _rotation;
+            float _elevation;
+            const float _rotation_speed = 0.05f;
             glm::vec3 _direction;
             glm::vec3 _normal;
 
+            void update();
+
         public:
             Camera(glm::vec3 eye_position, glm::vec3 direction, glm::vec3 normal);
+            Camera(glm::vec3 eye_position, float rotation, float elevation);
 
             glm::vec3 getEyePosition();
             glm::mat4 getViewMatrix();
+            void left(float time);
+            void right(float time);
+            void up(float time);
+            void down(float time);
 
     };
 }
